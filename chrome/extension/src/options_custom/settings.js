4cc9600eb619213eb2a2440616ce951d84173afb
Steve Maris <steve_maris@hotmail.com>
2018-01-23 14:41:22 +0100

    Add chrome extension

---
+++ chrome/extension/src/options_custom/settings.js
@@ -0,0 +1,7 @@
+window.addEvent("domready", function () {
+    new FancySettings.initWithManifest(function (settings) {
+        /*settings.manifest.myButton.addEvent("action", function () {
+            alert("You clicked me!");
+        });*/
+    });
+});
