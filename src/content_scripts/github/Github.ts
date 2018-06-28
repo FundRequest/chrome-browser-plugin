93808801a88f9e9e34c05290f012e569d96af1e7
Steve Maris <steve_maris@hotmail.com>
2018-06-28 17:34:45 +0200

    #455 Add observer to reinit components when github updates part of the page

--- src/content_scripts/github/Github.ts
+++ src/content_scripts/github/Github.ts
@@ -13,6 +13,15 @@ import Settings from "../../classes/Settings";
 export default class Github {
     private static instance: Github;
     private static currentGithubRequest: GithubRequest = null;
+    private static observer: MutationObserver = null;
+    private static currentUrl: string = null;
+    private static eventHub: Element = null;
+    private static events = {
+        pageReload: new CustomEvent('fnd:github:page:reload'),
+        sidebarChange: new CustomEvent('fnd:github:issue:sidebar:change'),
+        headerChange: new CustomEvent('fnd:github:issue:header:change'),
+        listChange: new CustomEvent('fnd:github:issues:change')
+    };
 
     /**
      * @desc Get a Github.class instance
@@ -30,41 +39,121 @@ export default class Github {
             let readyStateCheckInterval = setInterval(() => {
                 if (document.readyState === "complete") {
                     clearInterval(readyStateCheckInterval);
-
-                    Github.init();
-                    document.addEventListener('pjax:complete', () => {
-                        Github.init();
-                    });
+                    Settings.getSettings().then((settings) => {
+                        if (settings != null && settings['loaded']) {
+                            Github.init();
+                        }
+                    })
                 }
             }, 10);
         });
     }
 
-    private static async init() {
-        await Settings.getInstance(); //already init and wait for an Settings instance, else it will be called for each issue in the overview since the async call to get the api takes to long.
-        Github.currentGithubRequest = null; // reset this to null will remove it available for garbage collection
+    private static init() {
+        Github.currentUrl = location.href;
+        Github.eventHub = document.createElement('div');
+        if (!Github.observer) {
+            Github.observer = Github.addUpdateObserver();
+        }
         let platformId = Github.getCurrentPlatformId();
-
         if (platformId != null) {
             Github.currentGithubRequest = new GithubRequest(platformId);
             let props = [];
             props['githubRequest'] = Github.currentGithubRequest;
-            VueInitializer.createComponent(document.querySelector('#partial-discussion-sidebar'), 'fnd-sidebar-issue-funds discussion-sidebar-item discussion-sidebar-item--fnd', GithubSidebarWidget, props);
-            VueInitializer.createComponent(document.querySelector('#partial-discussion-header .gh-header-actions'), 'fnd-action-buttons', GithubButtons, props);
+            Github.initSidebar(props);
+            Github.initButtons(props);
         }
 
         if (Github.containsCurrentPageIssues()) {
-            let issues = document.querySelectorAll('.issues-listing ul.js-navigation-container.js-active-navigation-container [data-id]');
-            if (issues.length > 0) {
-                for (let i = 0; i < issues.length; i++) {
-                    let currentProps = [];
-                    let href = (<HTMLAnchorElement>issues[i].querySelector('.js-navigation-open')).href;
-                    let meta: HTMLElement = issues[i].querySelector('.issue-meta-section');
-                    currentProps['platformId'] = Github.getPlatformIdFromUrl(href);
-                    VueInitializer.createComponent(meta, 'fnd-meta-issue-funds', GithubOverviewItemFunds, currentProps);
+            Github.initOverview();
+        }
+
+        Github.eventHub.addEventListener(Github.events.pageReload.type, () => {
+            if (Github.currentUrl != location.href) {
+                Github.init();
+            } else {
+                for (let key in Github.events) {
+                    Github.eventHub.dispatchEvent(Github.events[key]);
                 }
             }
+        });
+    }
+
+    private static addUpdateObserver(): MutationObserver {
+        let nodesFound;
+        let observer = new MutationObserver((list) => {
+            list.forEach((item) => {
+                if (item.addedNodes) {
+                    if ((item.target as Element).id == 'js-repo-pjax-container') {
+                        Github.eventHub.dispatchEvent(Github.events.pageReload);
+                    } else {
+                        let addedNodes = Array.of(item.addedNodes);
+
+                        nodesFound = addedNodes.forEach((el, i) => {
+                            let element = el[i] as Element;
+                            if (element) {
+                                if (element.id == 'partial-discussion-header') {
+                                    Github.eventHub.dispatchEvent(Github.events.headerChange);
+                                } else if (element.id == 'partial-discussion-sidebar') {
+                                    Github.eventHub.dispatchEvent(Github.events.sidebarChange);
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        });
+        observer.observe(document.querySelector('#js-repo-pjax-container'), {
+            subtree: true,
+            childList: true
+        });
+
+        return observer;
+    }
+
+    private static initSidebar(props: any) {
+        let sidebar = VueInitializer.createComponent('#partial-discussion-sidebar', 'fnd-sidebar-issue-funds discussion-sidebar-item discussion-sidebar-item--fnd', GithubSidebarWidget, props);
+        Github.eventHub.addEventListener(Github.events.sidebarChange.type, () => {
+            sidebar ? VueInitializer.reinitComponent(sidebar) : null;
+        });
+    }
+
+    private static initButtons(props: any) {
+        let buttons = VueInitializer.createComponent('#partial-discussion-header .gh-header-actions', 'fnd-action-buttons', GithubButtons, props);
+        Github.eventHub.addEventListener(Github.events.headerChange.type, () => {
+            buttons ? VueInitializer.reinitComponent(buttons) : null;
+        });
+    }
+
+    private static initOverview() {
+        let issues = document.querySelectorAll('.issues-listing ul.js-navigation-container.js-active-navigation-container [data-id]');
+        let components = [];
+        if (issues.length > 0) {
+            for (let i = 0; i < issues.length; i++) {
+                let issue: HTMLElement = issues[i] as HTMLElement;
+                components[`${issue.id}`] = Github.initOverviewItem(issue);
+            }
+            Github.eventHub.addEventListener(Github.events.listChange.type, () => {
+                for (let i = 0; i < issues.length; i++) {
+                    let issue: HTMLElement = issues[i] as HTMLElement;
+
+                    if (components[`${issue.id}`]) {
+                        VueInitializer.reinitComponent(components[`${issue.id}`]);
+                    } else {
+                        Github.initOverviewItem(issue);
+                    }
+                }
+            });
         }
+    }
+
+    private static initOverviewItem(issue: HTMLElement) {
+        let currentProps = [];
+        let href = (<HTMLAnchorElement>issue.querySelector('.js-navigation-open')).href;
+        currentProps['platformId'] = Github.getPlatformIdFromUrl(href);
+        return VueInitializer.createComponent(
+            `.issues-listing #${issue.id} .issue-meta-section`,
+            'fnd-meta-issue-funds', GithubOverviewItemFunds, currentProps);
     }
 
     /**
