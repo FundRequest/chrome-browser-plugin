5f346142d3bfffdce082a6e969f7cadb50916ff1
Nico Peeters <nico@itwhiz.be>
2018-07-11 11:21:56 +0200

    #27 - Add null check on funds.fndFunds

--- src/classes/models/RequestFunds.ts
+++ src/classes/models/RequestFunds.ts
@@ -6,7 +6,7 @@ export default class RequestFunds {
     usdFunds: number;
 
     public static getFundsByAddress(funds: RequestFunds, address: string): RequestFund {
-        if(funds.fndFunds.tokenAddress.toLowerCase() == address.toLowerCase()) {
+        if(funds.fndFunds != null && funds.fndFunds.tokenAddress.toLowerCase() == address.toLowerCase()) {
             return {...funds.fndFunds};
         } else if(funds.otherFunds != null) {
             return {...funds.otherFunds.find(fund => fund.tokenAddress.toLowerCase() == address.toLowerCase())};
